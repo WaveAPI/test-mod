abf0a40d27d02b037bd96918e462bf6c18287f17
nullBlade <stepan.gaidukevich@gmail.com>
2023-03-11 15:37:45 +0200

    lang and model

--- src/main/java/org/waveapi/testmod/TestItem.java
+++ src/main/java/org/waveapi/testmod/TestItem.java
@@ -1,9 +1,14 @@
 package org.waveapi.testmod;
 
 import org.waveapi.api.content.items.WaveItem;
+import org.waveapi.api.content.items.models.ItemModel;
+import org.waveapi.api.content.items.models.SimpleItemModel;
 
 public class TestItem extends WaveItem {
     public TestItem() {
         super("test_item", TestMod.getInstance());
+        setModel(new SimpleItemModel("test_item.png"));
+
+        addTranslation("en_us", "Test Item");
     }
 }
