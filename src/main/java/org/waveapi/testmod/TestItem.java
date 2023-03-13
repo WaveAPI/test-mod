ca5bea2d99d114cb9e03fba84e784f1c56917982
nullBlade <stepan.gaidukevich@gmail.com>
2023-03-13 05:13:19 +0200

    damage on use

--- src/main/java/org/waveapi/testmod/TestItem.java
+++ src/main/java/org/waveapi/testmod/TestItem.java
@@ -4,6 +4,7 @@ import org.waveapi.api.content.items.WaveItem;
 import org.waveapi.api.content.items.WaveShapedRecipe;
 import org.waveapi.api.content.items.models.SimpleItemModel;
 import org.waveapi.api.math.Vector3;
+import org.waveapi.api.world.entity.DamageSource;
 import org.waveapi.api.world.entity.living.EntityPlayer;
 import org.waveapi.api.world.inventory.ItemStack;
 import org.waveapi.api.world.inventory.ItemUseResult;
@@ -15,7 +16,6 @@ public class TestItem extends WaveItem {
 
         setModel(new SimpleItemModel("test/test_item.png"));
         setTab(TestTab.getInstance());
-        setMaxStackSize(16);
 
         addTranslation("en_us", "Test Item");
 
@@ -34,6 +34,7 @@ public class TestItem extends WaveItem {
     public ItemUseResult onUse(ItemStack item, UseHand hand, EntityPlayer player) {
         item.setAmount(item.getAmount() - 1);
         player.setVelocity(new Vector3(0, 0.5, 3.5).rotateY(-player.getHeadYaw() * (float)Math.PI / 180f));
+        player.damage(DamageSource.GENERIC, 2);
         return ItemUseResult.SUCCESS;
     }
 }
