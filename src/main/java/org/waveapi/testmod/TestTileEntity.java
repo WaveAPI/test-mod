a8082e1a321f5df7330b0186180b91da438dd222
nullBlade <stepan.gaidukevich@gmail.com>
2023-03-27 18:28:35 +0300

    DIAMONDS

--- src/main/java/org/waveapi/testmod/TestTileEntity.java
+++ src/main/java/org/waveapi/testmod/TestTileEntity.java
@@ -1,5 +1,6 @@
 package org.waveapi.testmod;
 
+import org.waveapi.api.content.items.block.MinecraftBlocks;
 import org.waveapi.api.content.items.block.blockentities.DeltaTicking;
 import org.waveapi.api.content.items.block.blockentities.TileEntityCreation;
 import org.waveapi.api.content.items.block.blockentities.WaveTileEntity;
@@ -12,9 +13,15 @@ public class TestTileEntity extends WaveTileEntity implements DeltaTicking {
     public boolean firstTick = true;
     @Override
     public void tick(int i) {
+        getWorld().setBlockState(getPosition().add(0, 1, 0), MinecraftBlocks.DIAMOND_BLOCK.getDefaultState());
         if (firstTick) {
             TestMod.log.log("Test tile first ticked, is it on client? " + getWorld().isClientSide());
             firstTick = false;
         }
+    }
+
+    @Override
+    public int maxTicksUnticked() {
+        return 1;
     }
 }
