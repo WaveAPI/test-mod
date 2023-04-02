fa6a6e79c7a59f6992086fcc2f3b502ed921386b
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-02 09:35:55 +0300

    use ore instead

--- src/main/java/org/waveapi/testmod/TestBlock.java
+++ src/main/java/org/waveapi/testmod/TestBlock.java
@@ -7,9 +7,7 @@ import org.waveapi.api.content.items.block.blockentities.TileEntityBlock;
 import org.waveapi.api.content.items.block.blockentities.WaveTileEntity;
 import org.waveapi.api.content.items.block.model.SixSidedBlockModel;
 import org.waveapi.api.content.items.drop.Drop;
-import org.waveapi.api.content.items.drop.DropFunction;
-import org.waveapi.api.content.items.drop.ItemDrop;
-import org.waveapi.api.content.items.drop.SilkTouchDrop;
+import org.waveapi.api.content.items.drop.DropHelper;
 
 public class TestBlock extends WaveBlock implements TileEntityBlock {
     public TestBlock() {
@@ -18,7 +16,7 @@ public class TestBlock extends WaveBlock implements TileEntityBlock {
         setModels(new SixSidedBlockModel((String) null));
         setHardness(0.5f);
         setDrop(new Drop[]{
-                new SilkTouchDrop(getAsSimpleDrop(), new ItemDrop(TestMod.another_item, 1, 3).apply(DropFunction.ORE_FORTUNE))
+                DropHelper.ore(this, TestMod.another_item, 5, 10)
         });
 
         addTranslation("en_us", "The HMM");
