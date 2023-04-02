f187a79b230f740efeeecd37e2871204c5f72318
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-02 15:27:10 +0300

    onUse

--- src/main/java/org/waveapi/testmod/itemsblocks/TestBlock2.java
+++ src/main/java/org/waveapi/testmod/itemsblocks/TestBlock2.java
@@ -6,6 +6,9 @@ import org.waveapi.api.content.items.block.MinecraftBlocks;
 import org.waveapi.api.content.items.block.WaveBlock;
 import org.waveapi.api.content.items.block.model.SixSidedBlockModel;
 import org.waveapi.api.math.BlockPos;
+import org.waveapi.api.world.entity.living.EntityPlayer;
+import org.waveapi.api.world.inventory.ItemUseResult;
+import org.waveapi.api.world.inventory.UseHand;
 import org.waveapi.api.world.world.BlockState;
 import org.waveapi.api.world.world.World;
 import org.waveapi.testmod.TestMod;
@@ -27,8 +30,28 @@ public class TestBlock2 extends WaveBlock {
         addTranslation("en_us", "The HMM 2.0");
     }
 
+    private static int[][] ADD = new int[][] {
+            {1, 0, 0},
+            {-1, 0, 0},
+            {0, 1, 0},
+            {0, -1, 0},
+            {0, 0, 1},
+            {0, 0, -1},
+    };
+
     @Override
     public void onRandomTick(BlockState state, BlockPos pos, World world) {
+        for (int[] add : ADD) {
+            BlockPos p = pos.add(add[0], add[1], add[2]);
+            if (world.getBlockState(p).getBlock() == MinecraftBlocks.STONE) {
+                world.setBlockState(p, getDefaultState());
+            }
+        }
+    }
+
+    @Override
+    public ItemUseResult onUse(BlockState blockState, BlockPos pos, World world, EntityPlayer entityPlayer, UseHand useHand) {
         world.setBlockState(pos, MinecraftBlocks.DIAMOND_BLOCK.getDefaultState());
+        return ItemUseResult.SUCCESS;
     }
 }
