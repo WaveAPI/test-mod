e697a6078b31595af75e66a3f8f7065880db49dd
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-02 14:39:10 +0300

    random tick

--- src/main/java/org/waveapi/testmod/itemsblocks/TestBlock2.java
+++ src/main/java/org/waveapi/testmod/itemsblocks/TestBlock2.java
@@ -2,8 +2,12 @@ package org.waveapi.testmod.itemsblocks;
 
 
 import org.waveapi.api.content.items.block.BlockMaterial;
+import org.waveapi.api.content.items.block.MinecraftBlocks;
 import org.waveapi.api.content.items.block.WaveBlock;
 import org.waveapi.api.content.items.block.model.SixSidedBlockModel;
+import org.waveapi.api.math.BlockPos;
+import org.waveapi.api.world.world.BlockState;
+import org.waveapi.api.world.world.World;
 import org.waveapi.testmod.TestMod;
 import org.waveapi.testmod.TestTab;
 
@@ -15,9 +19,16 @@ public class TestBlock2 extends WaveBlock {
         setHardness(2.5f);
         setDrop();
 
+        enableRandomTick();
+
         makePickaxeEffective();
         setMiningLevelRequired(1);
 
         addTranslation("en_us", "The HMM 2.0");
+    }
+
+    @Override
+    public void onRandomTick(BlockState state, BlockPos pos, World world) {
+        world.setBlockState(pos, MinecraftBlocks.DIAMOND_BLOCK.getDefaultState());
     }
 }
