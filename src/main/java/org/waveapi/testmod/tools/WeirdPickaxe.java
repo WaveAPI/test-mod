5fea6d6bc54b51f1090dcc1fdd381d721053c091
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-03 06:14:22 +0300

    Ouch pickaxe

--- src/main/java/org/waveapi/testmod/tools/WeirdPickaxe.java
+++ src/main/java/org/waveapi/testmod/tools/WeirdPickaxe.java
@@ -2,6 +2,12 @@ package org.waveapi.testmod.tools;
 
 import org.waveapi.api.content.items.models.SimpleToolModel;
 import org.waveapi.api.content.items.tools.WavePickaxeItem;
+import org.waveapi.api.math.BlockPos;
+import org.waveapi.api.world.entity.DamageSource;
+import org.waveapi.api.world.entity.living.EntityLiving;
+import org.waveapi.api.world.inventory.ItemStack;
+import org.waveapi.api.world.world.BlockState;
+import org.waveapi.api.world.world.World;
 import org.waveapi.testmod.TestMod;
 import org.waveapi.testmod.TestTab;
 
@@ -13,5 +19,13 @@ public class WeirdPickaxe extends WavePickaxeItem {
         setTab(TestTab.getInstance());
 
         addTranslation("en_us", "Weird Pickaxe");
+    }
+
+    @Override
+    public boolean onPostMine(ItemStack itemStack, World world, BlockState blockState, BlockPos pos, EntityLiving entity) {
+
+        entity.damage(DamageSource.GENERIC, 0.5f);
+
+        return true;
     }
 }
